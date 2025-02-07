313bac026f201739828ac84a660221b20bcc88e1
arksworld <arunkr4ever@gmail.com>
2025-02-07 13:14:23 +0530

    Updated for unit test changes to disable eureka server registration

--- springgateway/src/test/java/org/arksworld/ecommerceapp/springgateway/SpringgatewayApplicationTests.java
+++ springgateway/src/test/java/org/arksworld/ecommerceapp/springgateway/SpringgatewayApplicationTests.java
@@ -2,8 +2,13 @@ package org.arksworld.ecommerceapp.springgateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"eureka.client.enabled=false",
+		"eureka.instance.enabled=false"
+})
 class SpringgatewayApplicationTests {
 
 	@Test
